5f58c4a92ab3428a9d6243635a10b9d702933a89
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-03-20 16:33:44 +0000

    region filter import, functional implementation is next

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ ob_start("ob_gzhandler");
 include 'DB.php';
 $DB = new DB(parse_ini_file('/home/http/private/db-eve.ini'));
 
+$regions = json_decode(file_get_contents('inc/regions.json'),true);
+
 $time = microtime();
 $time = explode(' ', $time);
 $time = $time[1] + $time[0];
