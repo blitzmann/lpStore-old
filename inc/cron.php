8b5ffc5f1d03d243d7439f46433ceae1c81b0a45
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-04-22 04:31:36 +0000

    added initial isk/lp tracking script. this is very barebones, should be cleaned up once I get an lpOffer calculations class working. It's meant to be run at regular intervals, and will provide history on isk/lp/item. Also, a modified database schema where every offer is unique and linked to the corp would work much better than current implementations, but I would like to finish data verification first.

---
+++ inc/cron.php
@@ -0,0 +1,177 @@
+<?php
+
+// this would be so much easier once I migrate to unique offers
+
+
+require dirname(__FILE__).'/../config.php';
+
+// we only track data from The Forge region
+$emdr->regionID = 10000002;
+
+// todo: check to see if we can connect to EMDR (makes sure EMDR-py is running)
+
+$marketMode = 'sell';
+$time = time();
+
+$req  = array();
+$done = array();
+$i = 0;
+
+foreach ($DB->qa('
+            SELECT      a.typeID, a.quantity, b.typeName, a.parentID
+            FROM        lpRequiredItems a
+            INNER JOIN  invTypes b ON (b.typeID = a.typeID)
+            INNER JOIN  lpStore c ON (c.storeID = a.parentID)', array()) AS $item) {  
+                $req[$item['parentID']][] = $item; }
+$DB->beginTransaction(); 
+
+foreach ($DB->qa('SELECT a.*, b.typeName FROM lpStore a INNER JOIN  invTypes b ON (a.typeID = b.typeID)
+', array()) AS $offer) {
+
+    // collect unique info
+    $reqArray = array();
+    $array = array();
+    if (isset($req[$offer['storeID']])){
+        foreach ($req[$offer['storeID']] AS $item) {
+            $reaArray[$item['typeID']] = $item['quantity']; }
+        ksort($reqArray);
+    }
+    array_push($array, $offer['typeID'], $offer['quantity'], $offer['lpCost'], $offer['iskCost'], $reqArray);
+    $md5 = md5(json_encode($array));
+    
+    if (!array_key_exists($md5, $done)) {
+        
+    
+        // if this particular offer has not been processed yet, do it and add it.
+
+        // calculations are the exact same as the main page, with useless info stripped
+        
+        $totalCost = $offer['iskCost'];
+        $req       = array(); // array that holds name of reuired items
+        $cached    = false;   // flag
+        $bpc       = false;   // flag
+        
+        // get pricing info on item
+        if ($price = $emdr->get($offer['typeID'])) {
+            $cached   = true;
+            $price    = json_decode($price, true);
+            $timeDiff = (time() - $price['orders']['generatedAt'])/60/60; // time difference in hours
+        }
+        
+        // set required items
+        if (isset($req[$offer['storeID']])){
+            $reqItems = $reqContainer[$offer['storeID']];}
+        else {
+            $reqItems = array(); }
+
+        $manReqItems = array();
+                    
+        // Get pricing info for required items
+        // todo: should we parse pricing data for required items with no cache of their own?
+        foreach ($reqItems AS $reqItem) {
+            if ($reqItem['quantity'] <= 0) {
+                continue; }
+
+            if ($rprice = $emdr->get($reqItem['typeID'])) {
+                $rprice = json_decode($rprice, true);
+                $totalCost = $totalCost + ($rprice['orders']['sell'][0] * $reqItem['quantity']);
+            }
+        }
+        
+        // Blueprints are special fucking buterflies
+        if (strstr($offer['typeName'], " Blueprint")) {
+            $bpc       = true;
+            $manTypeID = $DB->q1(' 
+                SELECT      ProductTypeID  
+                FROM        invBlueprintTypes
+                WHERE       blueprintTypeID = ?', array($offer['typeID']));
+            
+            // set pricing info as the manufactured item
+            if ($price = $emdr->get($manTypeID)) {
+                $price  = json_decode($price, true); 
+                $cached = true;
+            }
+            
+            // Here we merge bill of materials for blueprints (remembering to multiple qnt with # of BPC runs)
+            $manReqItems = array_merge(
+                // Get minerals needed
+                $DB->qa('
+                    SELECT t.typeID,
+                           t.typeName,
+                           ROUND(greatest(0,sum(t.quantity)) * (1 + (b.wasteFactor / 100))) * ? AS quantity
+                    FROM
+                      (SELECT invTypes.typeid typeID,
+                              invTypes.typeName typeName,
+                              quantity
+                       FROM invTypes,
+                            invTypeMaterials,
+                            invBlueprintTypes
+                       WHERE invTypeMaterials.materialTypeID=invTypes.typeID
+                        AND invBlueprintTypes.productTypeID = invTypeMaterials.typeID
+
+                         AND invTypeMaterials.TypeID=?
+                       UNION 
+                       SELECT invTypes.typeid typeid,
+                                    invTypes.typeName name,
+                                    invTypeMaterials.quantity*r.quantity*-1 quantity
+                       FROM invTypes,
+                            invTypeMaterials,
+                            ramTypeRequirements r,
+                            invBlueprintTypes bt
+                       WHERE invTypeMaterials.materialTypeID=invTypes.typeID
+                         AND invTypeMaterials.TypeID =r.requiredTypeID
+                         AND r.typeID = bt.blueprintTypeID
+                         AND r.activityID = 1
+                         AND bt.productTypeID=?
+                         AND r.recycle=1) t
+                    INNER JOIN invBlueprintTypes b ON (b.productTypeID = ?)
+
+                    GROUP BY t.typeid,
+                             t.typeName', array($offer['quantity'], $manTypeID, $manTypeID, $manTypeID)),
+                // Get extra items needed
+                $DB->qa('
+                    SELECT t.typeID AS    typeID,
+                        t.typeName AS     typeName,
+                        (r.quantity * ?) AS quantity
+                    FROM ramTypeRequirements r,
+                        invTypes t,
+                        invBlueprintTypes bt,
+                        invGroups g
+                    WHERE r.requiredTypeID = t.typeID
+                    AND r.typeID = bt.blueprintTypeID
+                    AND r.activityID = 1
+                    AND bt.productTypeID = ?
+                    AND g.categoryID != 16
+                    AND t.groupID = g.groupID', array($offer['quantity'], $manTypeID))); // append material needs to req items      
+            
+            foreach ($manReqItems AS $reqItem) {
+                if ($reqItem['quantity'] <= 0) {
+                    continue; }
+
+                if ($rprice = $emdr->get($reqItem['typeID'])) {
+                    $rprice = json_decode($rprice, true);
+                    $totalCost = $totalCost + ($rprice['orders']['sell'][0] * $reqItem['quantity']);
+                }
+            }
+        }     
+           
+        if (!$cached) {
+            $lp2isk = 0;
+            $profit = 0; }
+        else {
+            $profit = ($price['orders'][$marketMode][0]*$offer['quantity'] - $totalCost);
+            $lp2isk = $profit / $offer['lpCost']; 
+        }
+         
+        $done[$md5] = $lp2isk;
+        
+    }
+     
+    $DB->ea("INSERT INTO lpTracking (storeID, time, lp2isk) VALUES (?, NOW(), ?)", array($offer['storeID'], $done[$md5]));
+}
+$DB->commit();
+print "Yay!";
+
+?>
+
+
